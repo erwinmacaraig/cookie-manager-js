44e268e71e87b2de3518329d251026f677708f90
erwinmacaraig <mister.m.codes@gmail.com>
2020-01-03 09:15:08 +0800

    Initial commit
    Project structure
    Add project license.

---
+++ example/script.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    /* CookieManager.set('erwin', 'macaraig', 3);
+
+    CookieManager.set('chkizna', 'gelica', 5);
+
+    console.log(CookieManager.get('chkizna')); */
+
+    /* CookieManager.set('erwin', 'pinaka pogi', 10);
+    CookieManager.remove('erwin');
+     */
+    CookieManager.clear();
+};
